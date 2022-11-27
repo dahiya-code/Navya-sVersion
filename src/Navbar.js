818d12977ff8b01aa76eecd33a188e1b530799fc
dahiya-code <d.navya2312@gmail.com>
2022-11-27 11:12:41 +0530

    url change

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
       <h1>Navya's Version</h1>
       <div className="links">
         <Link to="/">New</Link>
-        <Link to="/create" style={{ 
+        <Link to="/search" style={{ 
           color: 'white', 
           backgroundColor: '#f1356d',
           borderRadius: '8px' 
